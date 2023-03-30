ecd20883d7c49759a8f6fba6dfc5a6cda5c2f400
Bo <omuqqz@gmail.com>
2023-03-30 16:06:43 +0300

    feat: add api interfaces

---
+++ weather/src/APIs/openWeatherApi.ts
@@ -0,0 +1,22 @@
+import { createUrlWithParameters } from "../helpers/createUrlWithParameters";
+
+const apiUrl = "http://api.openweathermap.org/geo/1.0/direct"
+const apiKey = "047bc0b3c4f6848e338883ef35ae4259";
+
+export type FoundedCityResponse = CityInfo[]
+
+export interface CityInfo {
+  name: string
+  lat: number
+  lon: number
+  country: string
+}
+
+export function getFindCityByNameUrl(name: string): string {
+    const parameters = {
+        q: name,
+        limit: 5,
+        appid: apiKey
+    }
+    return createUrlWithParameters(apiUrl, parameters);
+} 
