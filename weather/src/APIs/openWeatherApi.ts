552f147170ffa5c363781af612cb1e1182d0d419
Bo <omuqqz@gmail.com>
2023-03-31 14:15:30 +0300

    feat: modify some APIs

--- weather/src/APIs/openWeatherApi.ts
+++ weather/src/APIs/openWeatherApi.ts
@@ -3,7 +3,7 @@ import { createUrlWithParameters } from "../helpers/createUrlWithParameters";
 const apiUrl = "http://api.openweathermap.org/geo/1.0/direct"
 const apiKey = "047bc0b3c4f6848e338883ef35ae4259";
 
-export type FoundedCityResponse = CityInfo[]
+export type FoundedCitiesResponse = CityInfo[]
 
 export interface CityInfo {
   name: string
--- weather/src/APIs/openmeteoApi.ts
+++ weather/src/APIs/openmeteoApi.ts
@@ -43,20 +43,20 @@ export function getWeatherType(weatherCode: number): WeatherType {
         case 61:
         case 63:
         case 65:
-        case 66:
-        case 67:
         case 80:
         case 81:
         case 82: return WeatherType.Rain;
+        case 66:
+        case 67: return WeatherType.SnowRain;
         case 71:
         case 73:
         case 75:
         case 85:
         case 86: return WeatherType.Snow;
         case 95:
-        case 96:
-        case 99: return WeatherType.Thunder;
-        default: return WeatherType.Fog;
+        case 96: return WeatherType.Thunderstorm;
+        case 99: return WeatherType.Hail;
+        default: return WeatherType.Unknown;
     }
 }
 
