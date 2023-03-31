a13c339fde8c08ed99cef3f04247b2e048e1e0ae
Bo <omuqqz@gmail.com>
2023-03-31 14:23:12 +0300

    feat: add styled-components

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
 
 function App(): JSX.Element {
-
   return (
     <div className="App">
-      
+
     </div>
   );
 }
--- weather/src/index.tsx
+++ weather/src/index.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import { store } from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
