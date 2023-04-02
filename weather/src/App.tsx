34a3d83154aca178df1cb7fd5ad3fc8164158de3
Bo <omuqqz@gmail.com>
2023-04-03 01:52:32 +0300

    feat: create weather page component

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,11 +1,35 @@
 import './App.css';
 import React from 'react';
+import styled from 'styled-components';
+import WeatherPage from './components/WeatherPage';
+
+const AppWrapper = styled.div`
+  height: 100vh;
+  box-sizing: border-box;
+  padding-top: 10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+  .content {
+    width: 90%;
+    max-width: 1200px;
+    display: flex;
+    border-radius: 5px;
+    box-sizing: border-box;
+    background-color: rgba(243, 243, 243, 0.85);
+  }
+`
+
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-
-    </div>
+    <AppWrapper>
+      <div className="content">
+        <WeatherPage/>
+      </div>
+    </AppWrapper>
   );
 }
 
