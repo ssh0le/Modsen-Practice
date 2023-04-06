1a6ce4c06b66b9f7c2d94723da410cf9e5ee2983
Bo <omuqqz@gmail.com>
2023-04-07 02:25:00 +0300

    feat: modify forecast slice

--- weather/src/store/forecastSlice/index.ts
+++ weather/src/store/forecastSlice/index.ts
@@ -1,44 +1,48 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { DailyForecastResponse } from "@api/dailyForecastApi";
 import { HourlyForecastResponse } from "@api/hourlyForecastApi";
+import { ForecastGeolocation } from "@global/types";
 
 interface WeatherForecastState {
-    isLoading: boolean,
-    daily: DailyForecastResponse | null,
-    hourly: HourlyForecastResponse | null
-    isError: boolean,
+   fetchTime: string | null,
+   isLoading: boolean,
+   daily: DailyForecastResponse | null,
+   hourly: HourlyForecastResponse | null
+   isError: boolean,
 }
 
 const initialState: WeatherForecastState = {
-    isLoading: true,
-    daily: null,
-    hourly: null,
-    isError: false
+   fetchTime: null,
+   isLoading: true,
+   daily: null,
+   hourly: null,
+   isError: false
 }
 
 const weatherForecastSlice = createSlice({
-    name: 'forecast',
-    initialState,
-    reducers: {
-         startFetch: (state) => {
-            state.isLoading = true;
-            state.isError = false;
-         },
-         setDailyForecast: (state, action: PayloadAction<DailyForecastResponse>) => {
-            state.daily = action.payload;
-         },
-         setHourlyForecast: (state, action: PayloadAction<HourlyForecastResponse>) => {
-            state.hourly = action.payload;
-         },
-         fetchSucces: (state) => {
-            state.isLoading = false;
-         },
-         fetchFailed: (state) => {
-            state.isError = true;
-            state.isLoading = false;
-         }
-    }
+   name: 'forecast',
+   initialState,
+   reducers: {
+      startFetch: (state, action: PayloadAction<ForecastGeolocation>) => {
+         state.isLoading = true;
+         state.isError = false;
+      },
+      setDailyForecast: (state, action: PayloadAction<DailyForecastResponse>) => {
+         state.daily = action.payload;
+      },
+      setHourlyForecast: (state, action: PayloadAction<HourlyForecastResponse>) => {
+         state.hourly = action.payload;
+      },
+      fetchSucces: (state) => {
+         state.isLoading = false;
+         state.fetchTime = new Date().toString();
+      },
+      fetchFailed: (state) => {
+         state.isError = true;
+         state.isLoading = false;
+      }
+   }
 })
 
-export const {startFetch, setDailyForecast, setHourlyForecast, fetchSucces, fetchFailed} = weatherForecastSlice.actions;
+export const { startFetch, setDailyForecast, setHourlyForecast, fetchSucces, fetchFailed } = weatherForecastSlice.actions;
 export default weatherForecastSlice.reducer;
