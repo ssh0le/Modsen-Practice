6c21a62d655b020f06743744b75d977db62ede4c
Bo <omuqqz@gmail.com>
2023-04-07 02:23:17 +0300

    feat: modify some selectors

--- weather/src/store/selectors/index.ts
+++ weather/src/store/selectors/index.ts
@@ -1,10 +1,13 @@
 import { RootState } from "..";
 import { Day } from "@api/dailyForecastApi";
+import { Hourly } from "@api/hourlyForecastApi";
 
 export const selectGeolocation = (state: RootState) => state.location.geolocation;
 export const selectLocationInfo = (state: RootState) => state.location.locationInfo;
 
+export const selectFetchTime = (state: RootState) => state.forecast.fetchTime;
+
 export const selectDaily = (state: RootState): Day[] | undefined => state.forecast.daily?.daily.data;
 export const selectToday = (state: RootState) => state.forecast.daily?.daily.data[0];
 
-export const selectHourly = (state: RootState) => state.forecast.hourly?.hourly;
+export const selectHourly = (state: RootState): Hourly | undefined => state.forecast.hourly?.hourly;
--- weather/src/store/selectors/selectForecast.ts
+++ weather/src/store/selectors/selectForecast.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { createSelector } from "@reduxjs/toolkit";
-import { selectDaily } from ".";
+import { createSelector} from "@reduxjs/toolkit";
+import { selectDaily, selectHourly } from ".";
 import { ForecastListItem } from "@global/types";
 import { getShortDayName } from "@helpers/getShortDayName";
 import { getWeatherType } from "@api/dailyForecastApi";
+import { getWeatherType as getHourlyWeatherType } from "@api/hourlyForecastApi";
+import { getFormattedTime } from "@helpers/getFormattedTime";
 
 
 export const selectDailyForecast = createSelector(selectDaily, dailyForecast => {
@@ -20,4 +22,28 @@ export const selectDailyForecast = createSelector(selectDaily, dailyForecast => 
     });
     return array;
 });
+
+export const selectHourlyForecast = createSelector(selectHourly, hourlyForecast => {
+    if (hourlyForecast === undefined) {
+        return [];
+    }
+    const array: ForecastListItem[] = [];
+    const {time, temperature_2m, weathercode} = hourlyForecast;
+    const today = new Date();
+    const currentHours = today.getHours();
+    const currentDay = today.getDate();
+    const index = time.findIndex(item => {
+        const forecastDate = new Date(item);
+        return forecastDate.getHours() === currentHours && forecastDate.getDate() === currentDay;
+    });
+    console.log(index);
+    for (let i = 0; i < 24; i++) {
+        array.push({
+            title: getFormattedTime(new Date(time[index + i])),
+            weatherType: getHourlyWeatherType(weathercode[index + i]),
+            temperature: Math.round(temperature_2m[index + i])
+        })
+    }
+    return array;
+})
 
