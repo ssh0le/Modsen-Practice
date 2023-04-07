518ea5ef6f6b2c6bff0c2931b743490aee4f3b48
Bo <omuqqz@gmail.com>
2023-04-08 02:52:34 +0300

    feat: add day period in selectors

--- weather/src/store/selectors/selectForecast.ts
+++ weather/src/store/selectors/selectForecast.ts
@@ -1,21 +1,23 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { createSelector } from "@reduxjs/toolkit";
-import { selectDaily, selectHourly, selectTimeZone } from ".";
-import { ForecastListItem, WeatherType } from "@global/types";
+import { selectDaily, selectHourly, selectSunrises, selectSunsets, selectTimeZone } from ".";
+import { DayForecast, DayPeriod, HourForecast, WeatherType } from "@global/types";
 import { getShortDayName } from "@helpers/getShortDayName";
 import { getWeatherType } from "@api/dailyForecastApi";
 import { getWeatherType as getHourlyWeatherType } from "@api/hourlyForecastApi";
 import { getFormattedTime } from "@helpers/getFormattedTime";
 import { getTimeZonedCurrentDate } from "@helpers/getTimeZonedCurrentDate";
+import { isSameDates } from "@helpers/isSameDates";
+import { getDayPeriod } from "@helpers/getDayPeriod";
 
 export const selectDailyForecast = createSelector(selectDaily, dailyForecast => {
     if (dailyForecast === undefined) {
         return [];
     }
-    const array: ForecastListItem[] = [];
+    const array: DayForecast[] = [];
     dailyForecast.forEach(({ day, all_day }) => {
         array.push({
-            title: getShortDayName(new Date(day)),
+            time: getShortDayName(new Date(day)),
             weatherType: getWeatherType(all_day.icon),
             temperature: Math.round(all_day.temperature_max)
         })
@@ -23,22 +25,29 @@ export const selectDailyForecast = createSelector(selectDaily, dailyForecast => 
     return array;
 });
 
-export const selectHourlyForecast = createSelector(selectHourly, selectTimeZone, (hourlyForecast, timeZone) => {
+export const selectHourlyForecast = createSelector(selectHourly, selectTimeZone, selectSunrises, selectSunsets, (hourlyForecast, timeZone, sunrises, sunsets) => {
     if (hourlyForecast === undefined) {
         return [];
     }
-    const array: ForecastListItem[] = [];
-    const { time, temperature_2m, weathercode } = hourlyForecast;
+    const array: HourForecast[] = [];
+    const { time: dates, temperature_2m, weathercode } = hourlyForecast;
     const today = getTimeZonedCurrentDate(timeZone);
-    const index = time.findIndex(item => {
+    const timeIndex = dates.findIndex(item => {
         const forecastDate = new Date(item);
         return forecastDate.getHours() === today.getHours() && forecastDate.getDate() === today.getDate();
     });
+    const sunIndex = sunrises?.findIndex(item => isSameDates(today, new Date(item)));
     for (let i = 0; i < 24; i++) {
+        const time = new Date(dates[timeIndex + i]);
+        let dayPeriod = DayPeriod.Day;
+        if (sunrises !== undefined && sunsets !== undefined&& sunIndex !== undefined) {
+            dayPeriod = getDayPeriod(new Date(sunrises[sunIndex]), new Date(sunsets[sunIndex]), time);
+        }
         array.push({
-            title: getFormattedTime(new Date(time[index + i])),
-            weatherType: getHourlyWeatherType(weathercode[index + i]),
-            temperature: Math.round(temperature_2m[index + i])
+            time: getFormattedTime(time),
+            weatherType: getHourlyWeatherType(weathercode[timeIndex + i]),
+            temperature: Math.round(temperature_2m[timeIndex + i]),
+            dayPeriod 
         })
     }
     return array;
