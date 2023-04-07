21e0edbe0c26542be04a487162957ebe9625f768
Bo <omuqqz@gmail.com>
2023-04-08 01:14:12 +0300

    feat: add todayForecast, update selectors

--- weather/src/store/selectors/selectForecast.ts
+++ weather/src/store/selectors/selectForecast.ts
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { createSelector} from "@reduxjs/toolkit";
-import { selectDaily, selectHourly } from ".";
-import { ForecastListItem } from "@global/types";
+import { createSelector } from "@reduxjs/toolkit";
+import { selectDaily, selectHourly, selectTimeZone } from ".";
+import { ForecastListItem, WeatherType } from "@global/types";
 import { getShortDayName } from "@helpers/getShortDayName";
 import { getWeatherType } from "@api/dailyForecastApi";
 import { getWeatherType as getHourlyWeatherType } from "@api/hourlyForecastApi";
 import { getFormattedTime } from "@helpers/getFormattedTime";
-
+import { getTimeZonedCurrentDate } from "@helpers/getTimeZonedCurrentDate";
 
 export const selectDailyForecast = createSelector(selectDaily, dailyForecast => {
     if (dailyForecast === undefined) {
         return [];
     }
-    const array: ForecastListItem[] = []; 
-    dailyForecast.forEach(({day, all_day}) => {
+    const array: ForecastListItem[] = [];
+    dailyForecast.forEach(({ day, all_day }) => {
         array.push({
             title: getShortDayName(new Date(day)),
             weatherType: getWeatherType(all_day.icon),
@@ -23,20 +23,17 @@ export const selectDailyForecast = createSelector(selectDaily, dailyForecast => 
     return array;
 });
 
-export const selectHourlyForecast = createSelector(selectHourly, hourlyForecast => {
+export const selectHourlyForecast = createSelector(selectHourly, selectTimeZone, (hourlyForecast, timeZone) => {
     if (hourlyForecast === undefined) {
         return [];
     }
     const array: ForecastListItem[] = [];
-    const {time, temperature_2m, weathercode} = hourlyForecast;
-    const today = new Date();
-    const currentHours = today.getHours();
-    const currentDay = today.getDate();
+    const { time, temperature_2m, weathercode } = hourlyForecast;
+    const today = getTimeZonedCurrentDate(timeZone);
     const index = time.findIndex(item => {
         const forecastDate = new Date(item);
-        return forecastDate.getHours() === currentHours && forecastDate.getDate() === currentDay;
+        return forecastDate.getHours() === today.getHours() && forecastDate.getDate() === today.getDate();
     });
-    console.log(index);
     for (let i = 0; i < 24; i++) {
         array.push({
             title: getFormattedTime(new Date(time[index + i])),
@@ -47,3 +44,18 @@ export const selectHourlyForecast = createSelector(selectHourly, hourlyForecast 
     return array;
 })
 
+export const selectTodayForecast = createSelector(selectDaily, dailyForecast => {
+    if (dailyForecast === undefined) {
+        return {
+            weatherType: WeatherType.Unknown,
+            temperature: 0,
+            summary: "No info"
+        };
+    }
+    const todayForecast = dailyForecast[0];
+    return {
+        weatherType: getWeatherType(todayForecast.icon),
+        temperature: Math.round(todayForecast.all_day.temperature_max),
+        summary: todayForecast.summary
+    }
+})
