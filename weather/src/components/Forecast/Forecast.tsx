d4a5d2173f548fab5389d0a3e6e787168fa6f845
Bo <omuqqz@gmail.com>
2023-04-10 01:01:19 +0300

    feat: modify forecast component

--- weather/src/components/Forecast/Forecast.tsx
+++ weather/src/components/Forecast/Forecast.tsx
@@ -4,13 +4,15 @@ import ForecastSwitch from "./ForecastSwitch";
 import DailyForecast from "./DailyForecast";
 import HourlyForecast from "./HourlyForecast";
 import { useUpdateForecast } from "@hooks/useUpdateForecast";
+import { useAppSelector } from "@hooks/storeHooks";
+import Loader from "@components/Loader";
 
 
 const ForecastWrapper = styled.div`
     display: flex;
     flex-direction: column;
     gap: 30px;
-    .switch {
+    .switch, .loader {
         display: flex;
         justify-content: center;
     }
@@ -19,7 +21,7 @@ const ForecastWrapper = styled.div`
 const Forecast: FC = () => {
     useUpdateForecast();
     const [selectedOption, setSelectedOption] = useState(0);
-
+    const isLoading = useAppSelector(state => state.forecast.isLoading);
     const onOptionClickHandler = (index: number): MouseEventHandler<HTMLLIElement> => {
         return (event: MouseEvent<HTMLLIElement>) => {
             setSelectedOption(index);
@@ -30,8 +32,9 @@ const Forecast: FC = () => {
         <div className="switch">
             <ForecastSwitch selectedId={selectedOption} onClick={onOptionClickHandler}/>
         </div>
-        {selectedOption === 0 && <DailyForecast/>}
-        {selectedOption === 1 && <HourlyForecast/>}
+        {isLoading && <div className="loader"><Loader/></div>}
+        {!isLoading && selectedOption === 0 && <DailyForecast/>}
+        {!isLoading && selectedOption === 1 && <HourlyForecast/>}
     </ForecastWrapper>)
 }
 
