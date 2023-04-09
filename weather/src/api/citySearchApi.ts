9d3e725747f76a6066055ee6c2e6b18283e1e5a9
Bo <omuqqz@gmail.com>
2023-04-09 03:23:51 +0300

    feat: modify city search api

--- weather/src/api/citySearchApi.ts
+++ weather/src/api/citySearchApi.ts
@@ -1,4 +1,5 @@
 import { createUrlWithParameters } from "../helpers/createUrlWithParameters";
+import { fetchData } from "@helpers/fetchData";
 
 const apiUrl = "http://api.openweathermap.org/geo/1.0/direct"
 const apiKey = "047bc0b3c4f6848e338883ef35ae4259";
@@ -9,14 +10,14 @@ export interface CityInfo {
   name: string
   lat: number
   lon: number
-  country: string
+  country: string,
 }
 
-export function getFoundedCitiesUrl(name: string): string {
+export async function getFoundedCities(name: string): Promise<FoundedCitiesResponse> {
     const parameters = {
         q: name,
         limit: 5,
         appid: apiKey
     }
-    return createUrlWithParameters(apiUrl, parameters);
+    return await fetchData<FoundedCitiesResponse>(createUrlWithParameters(apiUrl, parameters));
 } 
