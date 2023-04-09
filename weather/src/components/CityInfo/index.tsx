8ad43148bf9b2c53f028a0e3f1b9fa21be0e650d
Bo <omuqqz@gmail.com>
2023-04-10 01:03:53 +0300

    feat: modify city info component

--- weather/src/components/CityInfo/index.tsx
+++ weather/src/components/CityInfo/index.tsx
@@ -1,16 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, MouseEvent } from 'react';
 import { City, Country, CityInfoContainer } from './styled';
 
 interface CityInfoProps {
   name: string;
   country: string;
+  onClick: (event: MouseEvent<HTMLLIElement>) => void;
+  onMouseDown: (event: MouseEvent<HTMLLIElement>) => void;
 }
 
-const CityInfo: FC<CityInfoProps> = ({ name, country }) => {
+const CityInfo: FC<CityInfoProps> = ({ name, country, onClick, onMouseDown }) => {
   return (
-    <CityInfoContainer>
+    <CityInfoContainer onClick={onClick} onMouseDown={onMouseDown}>
+      <Country>{country}, </Country>
       <City>{name}</City>
-      <Country>{country}</Country>
     </CityInfoContainer>
   );
 };
--- weather/src/components/CityInfo/styled.ts
+++ weather/src/components/CityInfo/styled.ts
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
 export const CityInfoContainer = styled.li`
+    margin: none;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 5px;
+    &:hover {
+        background-color: #f93;
+    }
 `
 
 export const Country = styled.span`
