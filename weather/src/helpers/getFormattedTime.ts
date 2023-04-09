9030b9cbfd82fbc26638e1bb19776b8472a51aa1
Bo <omuqqz@gmail.com>
2023-04-10 00:59:14 +0300

    feat: modify formatted time helper

--- weather/src/helpers/getFormattedTime.ts
+++ weather/src/helpers/getFormattedTime.ts
@@ -1,5 +1,5 @@
-export function getFormattedTime(date: Date): string {
-    if (date.getHours() === new Date().getHours()) {
+export function getFormattedTime(date: Date, currentDate: Date): string {
+    if (date.getHours() === currentDate.getHours()) {
       return 'Now';
     }
     return `${date.getHours()}:00`;
